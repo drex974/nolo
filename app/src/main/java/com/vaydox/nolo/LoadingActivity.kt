09dc55940880fa91843a391145a38030704a2203
dych <dex.ir93@gmail.com>
2019-04-13 19:45:00 +0200

    Rename MainActivity to LoadingActivity

--- app/src/main/java/com/vaydox/nolo/LoadingActivity.kt
+++ app/src/main/java/com/vaydox/nolo/LoadingActivity.kt
@@ -3,7 +3,7 @@ package com.vaydox.nolo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoadingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
